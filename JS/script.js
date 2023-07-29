87e3ca127487784f1011dba3aa22f45d2a0dd264
ny-Sopheaktra <60089537+ny-Sopheaktra@users.noreply.github.com>
2023-07-29 15:36:22 +0700

    Responsive Commit

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,13 @@ function imgSlider(anything) {
 }
 
 function changeCircleColor(color) {
-  const circle = document.querySelector('.circle');
+  const circle = document.querySelector(".circle");
   circle.style.background = color;
+}
+
+function toggleMenu() {
+  var menuToggle = document.querySelector('.toggle');
+  var navigation = document.querySelector('.navigation');
+  menuToggle.classList.toggle('active');
+  navigation.classList.toggle('active');
 }
